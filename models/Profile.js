f929b44d6e29da73542dde5cc8b0b55c0a50f1ec
iamtheef <eftihis.riise@gmail.com>
2019-12-12 15:26:39 +0200

    added profile routes

--- models/Profile.js
+++ models/Profile.js
@@ -1,34 +1,34 @@
 const mongoose = require("mongoose");
 const profSchema = new mongoose.Schema({
   user: {
-    type: Schema.Types.ObjectId,
+    type: mongoose.Schema.Types.ObjectId,
     ref: "users"
   },
 
   posts: [
     {
-      type: Schema.Types.ObjectId,
+      type: mongoose.Schema.Types.ObjectId,
       ref: "Post"
     }
   ],
 
   upvoted: [
     {
-      type: Schema.Types.ObjectId,
+      type: mongoose.Schema.Types.ObjectId,
       ref: "Post"
     }
   ],
 
   downvoted: [
     {
-      type: Schema.Types.ObjectId,
+      type: mongoose.Schema.Types.ObjectId,
       ref: "Post"
     }
   ],
 
   hidden: [
     {
-      type: Schema.Types.ObjectId,
+      type: mongoose.Schema.Types.ObjectId,
       ref: "Post"
     }
   ],
--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,6 +1,33 @@
 const express = require("express");
 const router = express.Router();
+const passport = require("passport");
+const mongoose = require("mongoose");
+
+// Models
+
+const Profile = require("../../models/Profile");
+const User = require("../../models/User");
+
+// Routes
 
 router.get("/test", (req, res) => res.json({ msg: "profile works!" }));
+
+router.get(
+  "/",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    let errors = {};
+    Profile.findOne({ user: req.user.id })
+      .then(profile => {
+        if (!profile) {
+          errors.noProfile = "No profile for this user was found";
+          return res.status(404).json({ errors });
+        } else {
+          res.json(profile);
+        }
+      })
+      .catch(e => res.status(404).json(e));
+  }
+);
 
 module.exports = router;
