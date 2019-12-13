475fc58a11c3771b985f6cd655e8af437b1165e2
iamtheef <eftihis.riise@gmail.com>
2019-12-14 00:00:01 +0200

    Post model & routes

---
+++ models/Post.js
@@ -0,0 +1,65 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const Post = new Schema({
+  user: {
+    type: mongoose.Types.ObjectId,
+    ref: "users"
+  },
+  title: {
+    type: String,
+    required: true
+  },
+  body: {
+    type: String,
+    required: true
+  },
+
+  avatar: {
+    type: String
+  },
+
+  upvotes: [
+    {
+      user: {
+        type: mongoose.Types.ObjectId,
+        ref: "users"
+      }
+    }
+  ],
+
+  downvotes: [
+    {
+      user: {
+        type: mongoose.Types.ObjectId,
+        ref: "users"
+      }
+    }
+  ],
+
+  comments: [
+    {
+      user: {
+        type: mongoose.Types.ObjectId,
+        ref: "users"
+      },
+      date: {
+        type: Date,
+        default: Date.now
+      },
+      body: {
+        type: String,
+        required: true
+      },
+      avatar: {
+        type: String
+      }
+    }
+  ],
+  date: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+module.exports = Post = mongoose.model("Post", Post);
--- routes/api/posts.js
+++ routes/api/posts.js
@@ -1,6 +1,99 @@
 const express = require("express");
 const router = express.Router();
+const mongoose = require("mongoose");
+const passport = require("passport");
+const Post = require("../../models/Post");
+const User = require("../../models/User");
+const Profile = require("../../models/Profile");
+const validatePost = require("../../validation/post");
 
-router.get("/test", (req, res) => res.json({ message: "posts works!" }));
+router.post(
+  "/new",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    const { errors, isValid } = validatePost(req.body);
+
+    if (!isValid) return res.status(400).json(errors);
+
+    const newPost = new Post({
+      user: req.user.id,
+      title: req.body.title,
+      body: req.body.body,
+      avatar: req.user.avatar,
+      likes: [],
+      comments: []
+    });
+
+    newPost.save().then(post => {
+      res.json(post);
+    });
+  }
+);
+
+// All posts
+router.get("/all", (req, res) => {
+  Post.find()
+    .sort({ date: -1 })
+    .then(posts => res.json(posts))
+    .catch(e => res.json(e));
+});
+
+// A specific post
+router.get("/:post_id", (req, res) => {
+  Post.findById(req.params.post_id)
+    .then(post => {
+      res.json(post);
+    })
+    .catch(e => res.status(404).json(e));
+});
+
+// Delete post
+router.delete(
+  "/:post_id",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    Profile.findOne({ user: req.user.id }).then(profile => {
+      Post.findById(req.params.post_id)
+        .then(post => {
+          if (post.user.toString() !== req.user.id) {
+            return res.status(401).json({ Error: "User not authorized" });
+          }
+
+          post
+            .remove()
+            .then(() => res.json({ postDeleted: "Post deleted succesfully" }));
+        })
+        .catch(res.status(404).json({ notFound: "Post not found" }));
+    });
+  }
+);
+
+// Upvote/revoke upvote post
+router.post(
+  "/:post_id/upvote",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    Profile.findOne({ user: req.user.id })
+      .then(profile => {
+        Post.findById(req.params.post_id)
+          .then(post => {
+            if (!post.upvotes.includes(req.user.id)) {
+              post.upvotes.push(req.user.id);
+              post.save().then(post => res.json(post));
+            } else {
+              let removeIndex = post.upvotes.indexOf(req.user.id);
+              post.upvotes.splice(removeIndex, 1);
+              post.save().then(post => res.json(post));
+            }
+          })
+          .catch(res.status(404).json({ notFound: "Post not found" }));
+      })
+      .catch(e =>
+        res
+          .status(401)
+          .json({ unathorized: "You are unauthorized for this action" })
+      );
+  }
+);
 
 module.exports = router;
