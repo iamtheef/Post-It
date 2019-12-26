e16d730331d244064c9ab0c1b4f74d46254cb0ab
iamtheef <eftihis.riise@gmail.com>
2019-12-26 23:33:28 +0200

    new post section additions

--- client/src/Components/NewPost.js
+++ client/src/Components/NewPost.js
@@ -4,11 +4,38 @@ import useInputState from "../Hooks/useInputState";
 import axios from "axios";
 
 function NewPost() {
+  // communities hooks
   const [dropdown, toggleDropdown] = useToggle(false);
   const [communities, setCommunities] = useState([]);
-  const [error, setError] = useState(undefined);
   const [com, setCom] = useState(undefined);
   const [icon, setIcon] = useState(undefined);
+
+  // post section hooks
+  const [textPost, toggleTextPost] = useToggle(true);
+  const [mediaPost, toggleMediaPost] = useToggle(false);
+  const [linkPost, toggleLinkPost] = useToggle(false);
+
+  // error handling
+  const [error, setError] = useState(undefined);
+
+  // functions
+  const handlePostSection = e => {
+    if (e.target.name === "textPost") {
+      if (linkPost) toggleLinkPost();
+      if (mediaPost) toggleMediaPost();
+      if (!textPost) toggleTextPost();
+    }
+    if (e.target.name === "mediaPost") {
+      if (textPost) toggleTextPost();
+      if (linkPost) toggleLinkPost();
+      if (!mediaPost) toggleMediaPost();
+    }
+    if (e.target.name === "linkPost") {
+      if (mediaPost) toggleMediaPost();
+      if (textPost) toggleTextPost();
+      if (!linkPost) toggleLinkPost();
+    }
+  };
 
   const handleCommunityChange = e => {
     e.preventDefault();
@@ -25,11 +52,12 @@ function NewPost() {
   }, [setCommunities]);
 
   return (
+    // HEADER & TITLE
     <div className="container column is-8" style={{ marginTop: "60px" }}>
       <p className="header3">Create Post</p>
       <hr></hr>
-      <div className=" columns ">
-        <div className="column is-6 is-left">
+      <div className="columns levels">
+        <div className="column">
           <input
             className="input"
             type="text"
@@ -41,7 +69,11 @@ function NewPost() {
           <p className="help is-danger is-centered">{error && error}</p>
         </strong>
 
-        <div className={`dropdown ${dropdown && "is-active"} column is-right`}>
+        {/* COMMUNITITES DROPDOWN */}
+        <div
+          className={`dropdown ${dropdown && "is-active"}`}
+          style={{ marginRight: "15px", marginTop: "11px" }}
+        >
           <div className="dropdown-trigger">
             <button
               className="button"
@@ -49,19 +81,19 @@ function NewPost() {
               aria-controls="dropdown-menu"
               onClick={toggleDropdown}
             >
-              <span style={{ width: "170px" }}>
+              <div style={{ marginTop: "5px" }}>
+                {icon && (
+                  <figure className="image is-48x48 comLogo">
+                    <img
+                      className="is-rounded"
+                      src={icon}
+                      alt="community logo"
+                    ></img>
+                  </figure>
+                )}
+              </div>
+              <span style={{ width: "160px" }}>
                 {com ? com : "Choose community"}
-                <div>
-                  {icon && (
-                    <figure className="image is-32x32">
-                      <img
-                        className="is-rounded"
-                        src={icon}
-                        alt="community logo"
-                      ></img>
-                    </figure>
-                  )}
-                </div>
               </span>
 
               {/* <p className=" help is-danger">
@@ -94,13 +126,79 @@ function NewPost() {
         </div>
       </div>
 
-      <input
-        className="textarea"
-        placeholder="10 lines of textarea"
-        rows="10"
-      ></input>
-      <button class="button is-dark">Submit</button>
-      <button class="button is-light">Cancel</button>
+      {/* TABS */}
+
+      <div className="tabs is-centered is-boxed" onClick={handlePostSection}>
+        <ul style={{ podition: "relative" }}>
+          <li className={textPost && "is-active"}>
+            <a name="textPost">
+              <span className="icon is-small">
+                <i className="fa fa-font" aria-hidden="true"></i>
+              </span>
+              Text
+            </a>
+          </li>
+
+          <li className={mediaPost && "is-active"}>
+            <a name="mediaPost">
+              <span className="icon is-small">
+                <i className="fa fa-camera" aria-hidden="true"></i>
+              </span>
+              Image/Video
+            </a>
+          </li>
+          <li className={linkPost && "is-active"}>
+            <a name="linkPost">
+              <span className="icon is-small">
+                <i
+                  className="fa fa-link"
+                  aria-hidden="true"
+                  style={{ marginRight: "5px" }}
+                ></i>
+              </span>
+              Link
+            </a>
+          </li>
+        </ul>
+      </div>
+      {/*  text field  */}
+      <div style={{ display: textPost ? "block" : "none" }}>
+        <input className="textarea" placeholder="Text"></input>
+      </div>
+
+      {/* media field */}
+      <div style={{ display: mediaPost ? "block" : "none" }}>
+        <div class="field">
+          <div class="file">
+            <label class="file-label">
+              <input class="file-input" type="file" name="resume" />
+              <span class="file-cta">
+                <span class="file-icon">
+                  <i class="fas fa-upload"></i>
+                </span>
+                <span class="file-label">Normal file…</span>
+              </span>
+            </label>
+          </div>
+        </div>
+      </div>
+
+      {/* link field */}
+      <div style={{ display: linkPost ? "block" : "none" }}>
+        <input
+          className="textarea"
+          placeholder="URL(required)"
+          rows="10"
+        ></input>
+      </div>
+
+      {/* BUTTONS*/}
+      <div style={{ marginTop: "20px" }}>
+        <button className="button is-dark" style={{ marginRight: "10px" }}>
+          Submit
+        </button>
+        <button className="button is-light">Cancel</button>
+      </div>
     </div>
   );
 }
