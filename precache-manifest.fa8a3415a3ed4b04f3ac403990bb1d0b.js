904e07a5253bc8a82f4e6b0acf4a61dc12204bd4
iamtheef <eftihis.riise@gmail.com>
2020-01-11 22:03:17 +0200

    Updates

---
+++ precache-manifest.fa8a3415a3ed4b04f3ac403990bb1d0b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e9546082a3f8e3839db53d14c20caa46",
+    "url": "/.github.io/postit/index.html"
+  },
+  {
+    "revision": "75e6aeb9b3acc8cebd28",
+    "url": "/.github.io/postit/static/css/2.4b3ccb6c.chunk.css"
+  },
+  {
+    "revision": "a5709fc16bca979f3e85",
+    "url": "/.github.io/postit/static/css/main.1e66f4fc.chunk.css"
+  },
+  {
+    "revision": "75e6aeb9b3acc8cebd28",
+    "url": "/.github.io/postit/static/js/2.84ac2761.chunk.js"
+  },
+  {
+    "revision": "38188225595ebd0189a27d5d18aaa89f",
+    "url": "/.github.io/postit/static/js/2.84ac2761.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a5709fc16bca979f3e85",
+    "url": "/.github.io/postit/static/js/main.96daff7d.chunk.js"
+  },
+  {
+    "revision": "9d016ddbf0ea420bc3d5",
+    "url": "/.github.io/postit/static/js/runtime-main.f6b0c444.js"
+  }
+]);
